885a0a3d3d24d3227eb15af4bfff10f15a9b3945
Jason Maliquid <jasonm2822@gmail.com>
2025-01-04 22:44:58 +0800

    🚧 SplashScreen

---
+++ src/lib/components/container/splashScreen.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Logo from '@/assets/logo/Calorify.png?enhanced';
+</script>
+
+<div class="flex h-dvh w-dvw items-center justify-center">
+	<div
+		class="motion-scale-in-[0.5] motion-translate-x-in-[-120%] motion-translate-y-in-[-60%] motion-rotate-in-[-1080deg] motion-duration-[1.13s]/scale
+  motion-delay-[0.38s]/scale motion-duration-[1.50s]/translate motion-duration-[1.20s]/rotate motion-ease-spring-bouncier flex flex-col items-center"
+	>
+		<enhanced:img src={Logo} alt="Logo" class="size-40" />
+		<h1>Calorify</h1>
+		<p>Smart Calorie Tracker</p>
+	</div>
+</div>

72fc899401b655937adf86e9091fb8e6fb1ccac8
Jason Maliquid <jasonm2822@gmail.com>
2025-01-05 08:38:40 +0800

    💄Update splash screen logo and enhance text styles for better readability

--- src/lib/components/container/splashScreen.svelte
+++ src/lib/components/container/splashScreen.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import Logo from '@/assets/logo/Calorify.png?enhanced';
+	import Logo from '@/assets/logo/CalorifyNoLabel.png?enhanced';
 </script>
 
 <div class="flex h-dvh w-dvw items-center justify-center">
 	<div
-		class="motion-scale-in-[0.5] motion-translate-x-in-[-120%] motion-translate-y-in-[-60%] motion-rotate-in-[-1080deg] motion-duration-[1.13s]/scale
-  motion-delay-[0.38s]/scale motion-duration-[1.50s]/translate motion-duration-[1.20s]/rotate motion-ease-spring-bouncier flex flex-col items-center"
+		class="flex flex-col items-center motion-scale-in-[0.5] motion-translate-x-in-[-120%]
+  motion-translate-y-in-[-60%] motion-rotate-in-[-1080deg] motion-duration-[1.13s]/scale motion-duration-[1.20s]/rotate
+	motion-duration-[1.50s]/translate motion-delay-[0.38s]/scale motion-ease-spring-bouncier"
 	>
-		<enhanced:img src={Logo} alt="Logo" class="size-40" />
-		<h1>Calorify</h1>
-		<p>Smart Calorie Tracker</p>
+		<enhanced:img src={Logo} alt="Logo" class="size-40 md:size-48" />
+		<h1 class="font-sansSerif text-xl md:text-2xl">Calorify</h1>
+		<p class="font-sans text-lg md:text-xl">Smart Calorie Tracker</p>
 	</div>
 </div>

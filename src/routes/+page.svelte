bc856fb9d02a7257409ebd8c1257248de5da8caa
Jason Maliquid <jasonm2822@gmail.com>
2025-01-05 17:51:28 +0800

    ✨ Implement splash screen timer to transition to form view after 3.5 seconds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
 	import SplashScreen from '@/components/container/splashScreen.svelte';
+	import { onDestroy, onMount } from 'svelte';
+
+	let splashTimer: number;
+	let showSplash: boolean = true;
+
+	onMount(() => {
+		splashTimer = setInterval(() => {
+			showSplash = false;
+		}, 3500);
+
+		onDestroy(() => {
+			clearInterval(splashTimer);
+		});
+	});
 </script>
 
-<SplashScreen />
+{#if showSplash}
+	<svelte:component this={SplashScreen} />
+{:else}
+	<h1>Form</h1>
+{/if}

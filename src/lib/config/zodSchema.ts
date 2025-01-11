7095d1a956cd47b98da0397cd65edfdefbe4cf43
Jason Maliquid <jasonm2822@gmail.com>
2025-01-11 22:18:26 +0800

    🔧 Added signUpSchema

--- src/lib/config/zodSchema.ts
+++ src/lib/config/zodSchema.ts
@@ -5,4 +5,13 @@ export const logInSchema = z.object({
 	password: z.string().min(4)
 });
 
+export const signUpSchema = z.object({
+	firstName: z.string().min(2),
+	lastName: z.string().min(2),
+	gender: z.string(),
+	email: z.string().email(),
+	password: z.string().min(4),
+	confirmPassword: z.string().min(4)
+});
+
 export type logInSchema = z.infer<typeof logInSchema>;

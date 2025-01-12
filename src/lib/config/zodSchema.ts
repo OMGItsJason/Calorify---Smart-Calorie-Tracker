bcc418c373322309e2fc01d7fbe3874ff0128a33
Jason Maliquid <jasonm2822@gmail.com>
2025-01-12 18:01:13 +0800

    ♻️ Refractor signUpSchema with password confirmation validation.

--- src/lib/config/zodSchema.ts
+++ src/lib/config/zodSchema.ts
@@ -5,14 +5,19 @@ export const logInSchema = z.object({
 	password: z.string().min(4)
 });
 
-export const signUpSchema = z.object({
-	firstName: z.string().min(2),
-	lastName: z.string().min(2),
-	gender: z.string(),
-	email: z.string().email(),
-	password: z.string().min(4),
-	confirmPassword: z.string().min(4)
-});
+export const signUpSchema = z
+	.object({
+		firstName: z.string().min(2),
+		lastName: z.string().min(2),
+		email: z.string().email(),
+		gender: z.string(),
+		password: z.string().min(4),
+		confirmPassword: z.string().min(4)
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ['confirmPassword'],
+		message: 'Passwords do not match'
+	});
 
 export type logInSchema = z.infer<typeof logInSchema>;
 export type signUpSchema = z.infer<typeof signUpSchema>;
